b0a950abde2f3b3825a9f2e67564af4503c9ee48
hankrum42 <stefan.kolev.dantchev@gmail.com>
2020-06-28 04:22:44 +0300

    unit of work

---
+++ FootballInformationSystem/FootballInformationSystem.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using FootballInformationSystem.Data.Model;
+using FootballInformationSystem.Data.Repository;
+using System.Threading.Tasks;
+
+namespace FootballInformationSystem.Data.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IEfRepository<City> Cities { get; }
+
+        IEfRepository<Competition> Competitions { get; }
+
+        IEfRepository<Country> Countries { get; }
+
+        IEfRepository<Match> Matches { get; }
+
+        IEfRepository<Team> Teams { get; }
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ FootballInformationSystem/FootballInformationSystem.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,85 @@
+﻿using FootballInformationSystem.Data.Model;
+using FootballInformationSystem.Data.Repository;
+using FootballInformationSystem.Infrastructure;
+using System.Threading.Tasks;
+
+namespace FootballInformationSystem.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MsSqlDbContext context;
+        private readonly IEfRepository<City> cities;
+        private readonly IEfRepository<Competition> competitions;
+        private readonly IEfRepository<Country> countries;
+        private readonly IEfRepository<Match> matches;
+        private readonly IEfRepository<Team> teams;
+
+        public UnitOfWork(
+            MsSqlDbContext context,
+            IEfRepository<City> cities,
+            IEfRepository<Competition> competitions,
+            IEfRepository<Country> countries,
+            IEfRepository<Match> matches,
+            IEfRepository<Team> teams
+            )
+        {
+            Validated.NotNull(context, nameof(context));
+            Validated.NotNull(cities, nameof(cities));
+            Validated.NotNull(competitions, nameof(competitions));
+            Validated.NotNull(countries, nameof(countries));
+            Validated.NotNull(matches, nameof(matches));
+            Validated.NotNull(teams, nameof(teams));
+            this.context = context;
+            this.cities = cities;
+            this.competitions = competitions;
+            this.countries = countries;
+            this.matches = matches;
+            this.teams = teams;
+        }
+
+        public IEfRepository<City> Cities
+        {
+            get
+            {
+                return this.cities;
+            }
+        }
+
+        public IEfRepository<Competition> Competitions
+        {
+            get
+            {
+                return this.competitions;
+            }
+        }
+
+        public IEfRepository<Country> Countries
+        {
+            get
+            {
+                return this.countries;
+            }
+        }
+
+        public IEfRepository<Match> Matches
+        {
+            get
+            {
+                return this.matches;
+            }
+        }
+
+        public IEfRepository<Team> Teams
+        {
+            get
+            {
+                return this.teams;
+            }
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await context.SaveChangesAsync();
+        }
+    }
+}
