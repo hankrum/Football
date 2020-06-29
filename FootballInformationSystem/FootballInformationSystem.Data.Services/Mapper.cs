05546b6c9e0d7caa39d6b50b669d8b2a6bc82708
hankrum42 <stefan.kolev.dantchev@gmail.com>
2020-06-29 04:49:53 +0300

    mapper

---
+++ FootballInformationSystem/FootballInformationSystem.Data.Services/Mapper.cs
@@ -0,0 +1,26 @@
+﻿using FootballInformationSystem.Infrastructure;
+using System;
+using Dbo = FootballInformationSystem.Data.Model;
+using Dto = FootballInformationSystem.Data.Services.DtoModels;
+
+namespace FootballInformationSystem.Data.Services
+{
+    public interface IMapper
+    {
+        Dbo.City Map(Dto.City city);
+    }
+
+    public class Mapper : IMapper
+    {
+        public Dbo.City Map(Dto.City city)
+        {
+            Validated.NotNull(city, nameof(city));
+
+            return new Dbo.City
+            {
+                Id = city.Id,
+                Name = city.Name
+            };
+        }
+    }
+}
